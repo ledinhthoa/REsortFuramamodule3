84bd66e2b6cf1b58241da6158f1d1e3ea8ba1add
ledinhthoa <ledaiphong6@gmail.com>
2019-11-05 08:34:31 +0700

    add.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarHeaderComponent } from './navbar-header/navbar-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarHeaderComponent
   ],
   imports: [
     BrowserModule
